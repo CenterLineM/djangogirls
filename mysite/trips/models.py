ad0aeca2d45b19db0544249f2a548423767b291f
CenterLineM <joviankingeli@gmail.com>
2018-03-28 18:03:39 +0800

    編碼修正

--- mysite/trips/models.py
+++ mysite/trips/models.py
@@ -13,5 +13,5 @@ class Post(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-		return self.name.encode('utf8')
+		return self.title.encode('utf8')
 		#return self.title
