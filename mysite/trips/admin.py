33a2a9595dfd4708293ad5ab533843bf29b85fa2
CenterLineM <joviankingeli@gmail.com>
2018-04-03 19:12:54 +0800

    清單顯示

--- mysite/trips/admin.py
+++ mysite/trips/admin.py
@@ -11,5 +11,11 @@ from .models import PostPag
 # Register your models here.
 
 admin.site.register(Post)
-# 那入管理
-admin.site.register(PostPag)
+
+
+#v顯示日期和時間
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'slug', 'pub_date')
+
+# 納入管理
+admin.site.register(PostPag, PostAdmin)
